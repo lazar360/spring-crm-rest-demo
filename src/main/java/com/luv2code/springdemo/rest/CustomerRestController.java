3ae63afebe2b2898c5547f6abb1af371b42eca1c
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-01-02 08:56:15 +0100

    end commit

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -3,8 +3,12 @@ package com.luv2code.springdemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,6 +42,48 @@ public class CustomerRestController {
 			throw new CustomerNotFoundException("Customer id not found - " + customerId);
 		}
 		
+		//Happy path
 		return theCustomer;
 	}
+	
+	// add mapping for POST / customers - add new customers
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer theCustomer) {
+		
+		// set id to 0 force a save of new item. The spring method is saveOrUpdate.
+		
+		theCustomer.setId(0);
+		
+		customerService.saveCustomer(theCustomer);
+		
+		return theCustomer;
+	}
+	
+	//add mapping for put /customers - update an existing customer
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer theCustomer) {
+		
+		customerService.saveCustomer(theCustomer);
+		
+		return theCustomer;
+	}
+	
+	// add mapping for delete / customers - delete an existing customer
+	@DeleteMapping("/customers/{customerId}")
+	public String deleteCustomer(@PathVariable int customerId) throws Exception {
+		
+		Customer tempCustomer = customerService.getCustomer(customerId);
+		
+		if(tempCustomer == null) {
+			throw new CustomerNotFoundException("Customer id not found - " + customerId);
+		}
+		
+		//Happy path
+		customerService.deleteCustomer(customerId);
+		
+		return "Delete customer id : " + customerId;
+	}
+	
+	
+	
 }
